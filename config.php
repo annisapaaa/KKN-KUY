6b338a66412d14aef0cbea8ff8596dd0c1af9a97
zorgonlistanto <63364966+zorgonlistanto@users.noreply.github.com>
2021-06-14 10:12:19 +0700

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db=pg_connect('host=localhost port=5432 dbname=webkkn user=postgres password=nenekLampir0192');
-if( !$db ){
-    die("Gagal terhubung dengan database: " . pg_connect_error());
-}
-?>
